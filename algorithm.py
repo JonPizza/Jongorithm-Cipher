e3e5f8ed98fba463d89bc9dff617db59d230e5e2
Jon <45501389+JonPizza@users.noreply.github.com>
2020-01-17 09:28:56 -0500

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,21 @@
+import keys
+import sys
 
+if len(sys.argv) >= 3 and len(sys.argv[1]) == 64: 
+    key = sys.argv[1]
+else:
+    print(f'Usage:\npython3 {sys.argv[0]} <256 bit hex key> <plaintext filename>')
+    exit()
+
+xor = {
+    '00': '1',
+    '01': '0',
+    '10': '0',
+    '11': '1'
+}
+
+def XOR(b1, b2):
+    return xor[str(b1) + str(b2)]
+
+def rotate(blocks, key, round):
+    pass
